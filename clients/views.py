c424c14257b8536a69fae87b79adb2ddf5023076
Madhur Jain <105913642+madhur-xelix@users.noreply.github.com>
2024-09-19 16:48:14 +0100

    feat: add drf and serialize client model with view and url defined

--- clients/views.py
+++ clients/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from clients.models import Client
+from clients.serializers import ClientSerializer
 
 # Create your views here.
+class ClientViewSet(viewsets.ModelViewSet):
+    """
+    A simple ViewSet for viewing and editing accounts.
+    """
+    queryset = Client.objects.all()
+    serializer_class = ClientSerializer
